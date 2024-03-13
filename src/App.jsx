d9d219f38c6ebafb7c4d57f6902471be1349d6d7
Emily Raspe <emilyraspe@icloud.com>
2024-03-13 14:54:00 +0100

    create form, create handleSubmit function, we are missing reset and focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 import Form from "./Form/Form.jsx";
 
-function App() {
-  const [activities, setActivities] = useState([]);
-
-  const handleAddActivity = (newActivity) => {
-    setActivities([...activities, newActivity]);
-  };
-
+export default function App() {
   return (
     <>
       <Form onAddActivity={handleAddActivity} />
--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,36 +1,51 @@
 import { useState } from "react";
 
 export default function Form({ onAddActivity }) {
-  const [activity, setActivities] = useState({
+  const [formData, setFormData] = useState({
     name: "",
-    isForGoodWeather: true,
+    isForGoodWeather: false,
   });
 
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    const newValue = type === "checkbox" ? checked : value;
+  /*  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    const val = type === "checkbox" ? checked : value;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: val,
+    }));
+  } */
 
-    setActivity({
-      ...activity,
-      [name]: newValue,
-    });
-  };
+  function handleSubmit(event) {
+    event.preventDefault();
+    /* const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData); */
+    const data = {
+      name: event.target.elements.name.value,
+      checkbox: event.target.elements.isForGoodWeather.checked,
+    };
+    console.log(data);
 
-  const onAddActivity = (e) => {
-    e.preventDefault();
+    console.log("event target ", event.target.elements.name);
+    console.log("event target ", event.target.elements.isForGoodWeather);
 
-    onAddActivity(activity);
-    console.log("Activity:", activity);
-  };
+    onAddActivity(data);
 
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
   return (
     <>
-      <form onSubmit={onAddActivity}>
+      <form onSubmit={handleSubmit}>
         <h1>Add new Activity</h1>
-        <label>Name</label>
-        <input type="text"></input>
-        <label>Good-weather Activity</label>
-        <input type="checkbox" checked={activity.isForGoodWeather}></input>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name"></input>
+        <label htmlFor="isForGoodWeather">Good-weather Activity</label>
+        <input
+          type="checkbox"
+          id="isForGoodWeather"
+          name="isForGoodWeather"
+        ></input>
+
         <button type="submit">Submit</button>
       </form>
     </>
